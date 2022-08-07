15b4865555e2ea683a37bb75cbfaf176689f4f1f
Nivedita AP <niveditaprasannaraj@gmail.com>
2022-08-07 23:25:36 +0530

    update: updated js

--- script.js
+++ script.js
@@ -4,14 +4,36 @@ var currentPrice = document.querySelector("#current-price");
 const submitBtn = document.querySelector("#submit-btn");
 const outputStock = document.querySelector("#output");
 
-function calculateProfitAndLoss(intialPrice, currentPrice){
-    if(intialPrice < currentPrice){
-        console.log("Yay profit");
+
+function submitHandler(){
+    var inp = intialPrice.value;
+    var qty = stockQuantity.value;
+    var curr = currentPrice.value;
+
+    calculateProfitAndLoss(inp, qty, curr);
+}
+
+function calculateProfitAndLoss(intial, quantity, current){
+    if(intial < current){
+        var profit = (current - intial ) * quantity;
+        var profitPercentage = (profit/intial) * 100;
+        showOutput("The profit is " + profit + " and profit percentage is " + profitPercentage);
     }
+    else if(current < intial){
+        var loss = (intial - current ) * quantity;
+        var lossPercentage = (loss/intial) * 100;
+        showOutput("The loss is " + loss + " and loss percentage is " + lossPercentage);
+    }
+    else{
+        showOutput("No pain no gain");
+    }
+
+}
+
+function showOutput(message){
+    outputStock.innerHTML = message;
 }
 
 
+submitBtn.addEventListener("click", submitHandler);
 
-submitBtn.addEventListener("click", calculateProfitAndLoss);
-
-console.log(calculateProfitAndLoss(20,30));
